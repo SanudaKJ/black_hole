650238094459fbdcfcd162210ba9d6f51dfd0b78
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-01-23 00:40:13 +0530

    Create final page

--- lib/main.dart
+++ lib/main.dart
@@ -119,7 +119,7 @@ class Blackhole extends StatelessWidget {
                       width: 50,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
-                        color: Color.fromARGB(255, 161, 37, 18),
+                        color: const Color.fromARGB(255, 161, 37, 18),
                       ),
                     ),
                     Container(
@@ -127,7 +127,7 @@ class Blackhole extends StatelessWidget {
                       width: 50,
                       decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(50),
-                        color: Color.fromARGB(255, 222, 184, 172),
+                        color: const Color.fromARGB(255, 222, 184, 172),
                       ),
                     ),
                     Container(
@@ -139,6 +139,71 @@ class Blackhole extends StatelessWidget {
                       ),
                     ),
                   ],
+                ),
+              ),
+
+              //third screen
+
+              Center(
+                child: Image.asset(
+                  "assets/space3.png",
+                  width: 300,
+                  height: 300,
+                ),
+              ),
+              const Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                style: TextStyle(color: Colors.white, fontSize: 15),
+                textAlign: TextAlign.center,
+              ),
+
+              const SizedBox(
+                height: 50,
+              ),
+
+              GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Container(
+                    //button(SPACE DETAILS)
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.pinkAccent,
+                    ),
+                    child: const Text(
+                      "SPACAE DETAILS",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ),
+
+              //footer
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Container(
+                  width: double.infinity,
+                  height: 2,
+                  decoration: const BoxDecoration(
+                    color: Colors.white30,
+                  ),
+                ),
+              ),
+
+              const Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white),
+              ),
+
+              const Padding(
+                padding: EdgeInsets.all(15.0),
+                child: Center(
+                  child: Text(
+                    "© 2021 Black Hole. All Rights Reserved.",
+                    style: TextStyle(color: Colors.white, fontSize: 13),
+                  ),
                 ),
               ),
             ]),
