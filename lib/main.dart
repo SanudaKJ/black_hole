7fd2ebd86cdc8c20dbf844898c8e465c7a17c9b6
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-01-22 23:58:48 +0530

    Create second page

--- lib/main.dart
+++ lib/main.dart
@@ -65,20 +65,82 @@ class Blackhole extends StatelessWidget {
               const SizedBox(
                 height: 50,
               ),
-              Center(
-                child: Container(
-                  //button(SPACE DETAILS)
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.red,
-                  ),
-                  child: const Text(
-                    "SPACAE DETAILS",
-                    style: TextStyle(color: Colors.white, fontSize: 20),
+              GestureDetector(
+                onTap: () {},
+                child: Center(
+                  child: Container(
+                    //button(SPACE DETAILS)
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.red,
+                    ),
+                    child: const Text(
+                      "SPACAE DETAILS",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
                   ),
                 ),
-              )
+              ),
+
+              //second screen
+              Center(
+                child: Image.asset(
+                  "assets/space2.png",
+                  width: 300,
+                  height: 300,
+                ),
+              ),
+
+              const Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: const Color.fromARGB(255, 177, 55, 17),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color.fromARGB(255, 161, 37, 18),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: Color.fromARGB(255, 222, 184, 172),
+                      ),
+                    ),
+                    Container(
+                      height: 50,
+                      width: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(50),
+                        color: const Color.fromARGB(255, 8, 79, 137),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ]),
           ),
         ),
